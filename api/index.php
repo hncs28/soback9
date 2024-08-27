0ce19dbbc86deef9716ed53ed17328be9ab31573
namchaser <namblue28092002@gmail.com>
2024-08-27 11:33:34 +0700

    modify

--- api/index.php
+++ api/index.php
@@ -1,6 +1,7 @@
 <?php
  
 // Load the Laravel application
+require __DIR__ . "/../public/index.php";
 require __DIR__ . '/../vendor/autoload.php';
 $app = require_once __DIR__ . '/../bootstrap/app.php';
  
--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
-
+        NunoMaduro\Collision\Adapters\Laravel\CollisionServiceProvider::class,
         /*
          * Package Service Providers...
          */

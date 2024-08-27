484cfd9c84c96ec5616a96a8438547b8bab31992
namchaser <namblue28092002@gmail.com>
2024-08-27 11:45:51 +0700

    new2

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
  
 // Load the Laravel application
-require __DIR__ . '/../vendor/autoload.php';
+require __DIR__ . "/../public/index.php";
 $app = require_once __DIR__ . '/../bootstrap/app.php';
  
 // Run the application
